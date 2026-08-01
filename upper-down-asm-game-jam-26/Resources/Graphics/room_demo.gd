e155960da52ea5d65409eab5136e77c434950f36
MattiAnttila <matti.anttila86@gmail.com>
2026-08-01 18:57:59 +0300

    push stuff away
    push stuff away

--- upper-down-asm-game-jam-26/Resources/Graphics/room_demo.gd
+++ upper-down-asm-game-jam-26/Resources/Graphics/room_demo.gd
@@ -5,11 +5,11 @@ extends Node2D
 var white: =Color.hex(454545)
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	if isDark:
-		$CanvasGroup/TileMapLayer.modulate = Color.ALICE_BLUE		
-	else :
-		$CanvasGroup/TileMapLayer.modulate = white
+#func _ready() -> void:
+#	if isDark:
+#		$CanvasGroup/TileMapLayer.modulate = Color.ALICE_BLUE		
+#	else :
+#		$CanvasGroup/TileMapLayer.modulate = white
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
