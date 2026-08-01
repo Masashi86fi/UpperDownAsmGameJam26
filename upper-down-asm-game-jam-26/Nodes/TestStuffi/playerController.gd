93fcbb8e1df62ecce2dbc42a403d04666f163d6f
Tuomas Sihvo <prodoom@hotmail.com>
2026-08-01 13:46:13 +0300

    Ghost unlocking added
    - Trigger node for unlocking it
    - Can't swap character until the value changes

--- upper-down-asm-game-jam-26/Nodes/TestStuffi/playerController.gd
+++ upper-down-asm-game-jam-26/Nodes/TestStuffi/playerController.gd
@@ -3,21 +3,25 @@ extends Node2D
 @export var lightCharacter: CharacterBody2D
 @export var darkCharacter: CharacterBody2D
 @onready var playerCamera: Camera2D = $Camera2D
+var ghostUnlocked = false
 var activeCharacter: CharacterBody2D
 
 func _ready():
 	activeCharacter = lightCharacter
 	
+func _unlockGhost():
+	ghostUnlocked = true
 
 func _input(event):
 	if event.is_action_pressed("swap"):
-		if activeCharacter == lightCharacter:
-			activeCharacter = darkCharacter
-		else:
-			activeCharacter = lightCharacter
-		print("Swapped to ",  activeCharacter)
-		lightCharacter._swap()
-		darkCharacter._swap()
+		if ghostUnlocked:
+			if activeCharacter == lightCharacter:
+				activeCharacter = darkCharacter
+			else:
+				activeCharacter = lightCharacter
+			print("Swapped to ",  activeCharacter)
+			lightCharacter._swap()
+			darkCharacter._swap()
 	if(Input.is_action_just_pressed("jump") and activeCharacter.is_on_floor()):
 		print("JUMP!")
 		activeCharacter._jump()
