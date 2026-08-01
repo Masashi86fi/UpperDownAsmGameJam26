1eb53819dda0d9bdc48cbf343967186edf3a0d8d
Tuomas Sihvo <prodoom@hotmail.com>
2026-08-01 17:32:30 +0300

    LightCharacter better jump physics
    - Now you setup jump height, time and fall time

--- upper-down-asm-game-jam-26/Nodes/TestStuffi/lightCharacterController.gd
+++ upper-down-asm-game-jam-26/Nodes/TestStuffi/lightCharacterController.gd
@@ -3,16 +3,26 @@ extends CharacterBody2D
 # All these can be edited via inspector
 @export var acceleration = 500 #LightPlayer x-speed
 @export var deceleration = 50 #LightPlayer x-deceleration speed
-@export var maxSpeed = 200 #IGNORE, dead variable
-@export var gravity = 100 #LightPlayer Gravity
-@export var jump_force = 200 #LightPlayer Jumpforce
+#@export var maxFallSpeed = 1200 #FallSpeed for gravity
+@export var jumpHeightPixels: float = 20.0 #LightPlayer jump height
+@export var jumpPeakTime: float = 0.5 #LightPlayer time to reach jump height peak
+@export var jumpFallTime: float = 0.5 #LighPlayer time to fall back to starting y.height
+var gravity: float #Gravity for when jumping
+var fallGravity: float #Gravity after reaching jumpHeight
+var jumpForce: float #Calculated jumpforce
 @export var activeCharacter = true #Only change from inspector in level if wnat to start as ghost?
 var currentInteractable: Area2D = null #Used for interacting / lightSwitch trigger detection
+
+# Calculate gravities+jumpForce to reach jump peak and fall times mathematically
+func _ready():
+	gravity = (2.0 * jumpHeightPixels) / pow(jumpPeakTime, 2)
+	fallGravity = (2 * jumpHeightPixels) / pow(jumpFallTime, 2)
+	jumpForce = gravity * jumpPeakTime
 
 # JUmp light character
 func _jump():
 	if is_on_floor():
-		velocity.y = -jump_force
+		velocity.y = -jumpForce
 
 # Swap if activeCharacter or not
 func _swap():
@@ -27,8 +37,11 @@ func _process(_delta):
 func _physics_process(delta):
 	# Apply gravity
 	if !is_on_floor():
-		velocity.y += gravity * delta
-
+		if velocity.y < 0:
+			velocity.y += gravity * delta
+		else:
+			velocity.y += fallGravity * delta
+	
 	# X-axis movement
 	if activeCharacter:
 		var direction = Input.get_axis("left", "right")
