5c68a148261271471ebf0df18c42421da07dd7ed
MattiAnttila <matti.anttila86@gmail.com>
2026-08-02 01:20:52 +0300

    update
    update

--- upper-down-asm-game-jam-26/Nodes/TestStuffi/door.gd
+++ upper-down-asm-game-jam-26/Nodes/TestStuffi/door.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
-@onready var doorSprite = $StaticBody2D/Sprite2D # Reference to door sprite 
+@onready var doorSprite: Sprite2D = $StaticBody2D/Sprite2D # Reference to door sprite 
 @onready var doorCollision = $StaticBody2D/CollisionShape2D # Reference to door collision
+
 
 # Open door (triggered via keyholeTriggers)
 func _doorOpen():  
