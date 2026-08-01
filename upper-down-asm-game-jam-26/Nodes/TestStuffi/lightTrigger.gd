d362bf9c73dcb7459100c7690145c13b67886c58
Tuomas Sihvo <prodoom@hotmail.com>
2026-08-01 14:10:29 +0300

    LightTrigger (aka lantern etc) sprites
    - Sprite2D = LightSource like candle
    - LightSprite = the light itself in sprite form

--- upper-down-asm-game-jam-26/Nodes/TestStuffi/lightTrigger.gd
+++ upper-down-asm-game-jam-26/Nodes/TestStuffi/lightTrigger.gd
@@ -2,14 +2,17 @@ class_name LightSource
 extends Area2D
 
 @onready var collision = $CollisionShape2D
+@onready var sprite = $LightSprite
 @export var enabledAtStart: bool = false
 @export var respawnPoint: Node2D
 
 func _ready():
 	if enabledAtStart == false:
 		collision.set_deferred("disabled", true)
+		sprite.visible = true
 	else:
 		collision.set_deferred("disabled", false)
+		sprite.visible = false
 
 func _on_body_entered(body: CharacterBody2D):
 	if body.name == "DarkPlayer":
@@ -17,3 +20,4 @@ func _on_body_entered(body: CharacterBody2D):
 		
 func _toggleLight():
 		collision.set_deferred("disabled", !collision.disabled)
+		sprite.visible = !sprite.visible
