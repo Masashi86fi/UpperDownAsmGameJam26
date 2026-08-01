b178bf40c2d39585d5453172aaec09f46d18d990
Otso Kangas <kangas.otso@gmail.com>
2026-08-01 22:35:32 +0300

    Main menu sound effect callbacks hooked

--- upper-down-asm-game-jam-26/Nodes/UI/main_menu.gd
+++ upper-down-asm-game-jam-26/Nodes/UI/main_menu.gd
@@ -4,12 +4,22 @@ extends Control
 @onready var start_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/StartButton as Button
 @onready var exit_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/ExitButton as Button
 const MAIN_MENU_THEME = preload("res://Resources/Audio/Music/main_theme.ogg")
+const EXIT_HOVER = preload("res://Resources/Audio/SFX/menu/on_exit_hover.ogg")
+const HOVER = preload("res://Resources/Audio/SFX/menu/on_hover.ogg")
+const CLICK = preload("res://Resources/Audio/SFX/menu/on_click.ogg")
 
 @onready var start_level=preload("res://Nodes/Main/main.tscn") as PackedScene
 
 func _ready():
 	start_button.button_down.connect(on_start_pressed)
+	start_button.mouse_entered.connect(on_hover)
+	start_button.mouse_exited.connect(on_exit_hover)
+	start_button.button_down.connect(on_click)
+	
 	exit_button.button_down.connect(on_exit_pressed)
+	exit_button.mouse_entered.connect(on_hover)
+	exit_button.mouse_exited.connect(on_exit_hover)
+	exit_button.button_down.connect(on_click)
 	AudioManager.play_music(MAIN_MENU_THEME)
 	
 func on_start_pressed() -> void:
@@ -17,3 +27,12 @@ func on_start_pressed() -> void:
 	
 func on_exit_pressed() -> void:
 	get_tree().quit()
+	
+func on_hover():
+	AudioManager.play_menu_FX(HOVER)
+
+func on_exit_hover():
+	AudioManager.play_menu_FX(EXIT_HOVER)
+
+func on_click():
+	AudioManager.play_menu_FX(CLICK)
